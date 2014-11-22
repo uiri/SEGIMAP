15e01b85549d1002de57bbab2eab87d865fed0c4
Nikita Pekin <contact@nikitapek.in>
2014-11-22 18:54:55 -0500

    Implement header parsing for messages.

--- src/message.rs
+++ src/message.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use error::{
     Error, ImapResult, MessageDecodeError
 };
@@ -32,18 +34,48 @@ impl Message {
         let header = contents.next().unwrap();
         let contents: Vec<&str> = contents.collect();
 
-        /*println!("Header: {}", header);
-        for line in header.lines() {
-            let split = line.as_slice().splitn(1, ':');
-            match split {
-                Some("Recieved", _) => println!("RECEIVED"),
-                _ => { }
+        // Iterate over the lines of the header in reverse.
+        // If a line with leading whitespace is detected, it is merged to the line before it.
+        // This "unwraps" the header as indicated in RFC 2822 2.2.3
+        let mut new_header: Vec<String> = Vec::new();
+        let mut iterator = header.lines().rev();
+        loop {
+            let line = match iterator.next() {
+                Some(line) => line,
+                None => break
+            };
+            let mut field = line.to_string();
+            if line.as_slice().starts_with(" ") || line.as_slice().starts_with("\t") {
+                field = field.as_slice().trim_left_chars(' ').trim_left_chars('\t').to_string();
+                loop {
+                    let next = iterator.next().unwrap().to_string();
+                    let mut trimmed_next = next.as_slice().trim_left_chars(' ').trim_left_chars('\t').to_string();
+                    // Add a space between the merged lines.
+                    trimmed_next.push_str(" ".as_slice());
+                    trimmed_next.push_str(field.as_slice());
+                    field = trimmed_next;
+                    if !next.as_slice().starts_with(" ") && !next.as_slice().starts_with("\t") {
+                        break;
+                    }
+                }
             }
-        }*/
-        println!("Contents:");
-        for msg in contents.iter() {
-            //println!("{}\n-------------------------\n", msg);
+
+            new_header.push(field.to_string());
         }
+        let header: Vec<&String> = new_header.iter().rev().collect();
+
+        let mut header_fields = HashMap::new();
+        for line in header.iter() {
+            let split: Vec<&str> = line.as_slice().splitn(1, ':').collect();
+            header_fields.insert(split[0].clone(), split[1].slice_from(1).clone());
+        }
+        // Remove the "Received" key from the HashMap.
+        header_fields.remove(&"Received");
+        println!("Header: {}", header_fields);
+        println!("Contents:");
+        /*for msg in contents.iter() {
+            println!("{}\n-------------------------\n", msg);
+        }*/
 
         let message = Message {
             uid: uid,
